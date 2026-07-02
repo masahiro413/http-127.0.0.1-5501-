51f9a43ad045413fd3f96c894b25dd74ce7a9cf5
masahiro413 <masahirokano503@gmail.com>
2026-07-02 17:59:38 +0900

    Add footer and navigation menu to footer.php
    footer.php

---
+++ php/footer.php
@@ -0,0 +1,41 @@
+	<!-- footer-menu -->
+	<div id="footer-menu">
+		<div class="inner">
+			<div class="footer-logo"><a href="/">みなと歯科クリニック</a></div><!-- /footer-logo -->
+			<div class="footer-sub">痛みに配慮した、通いやすい地域のかかりつけ歯科</div><!-- /footer-sub -->
+
+			<nav class="footer-nav">
+				<ul class="footer-list">
+					<li class="menu-item"><a href="#">初めての方へ</a></li>
+					<li class="menu-item"><a href="#">診療案内</a></li>
+					<li class="menu-item"><a href="#">医院紹介</a></li>
+					<li class="menu-item"><a href="#">料金・保険</a></li>
+					<li class="menu-item"><a href="#">アクセス・予約</a></li>
+				</ul>
+			</nav>
+
+		</div><!-- /inner -->
+	</div><!-- /footer-menu -->
+
+
+
+	<!-- footer -->
+	<footer id="footer">
+		<div class="inner">
+			<div class="copy">&copy; Minato Dental Clinic All rights reserved.</div><!-- /copy -->
+			<div class="by">Presented by <a href="#">みなと歯科クリニック</a>
+			</div><!-- /by -->
+
+		</div><!-- /inner -->
+	</footer><!-- /footer -->
+
+	<div class="floating">
+		<a href="#"><i class="fas fa-chevron-up"></i></a>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<script src="js/script.js"></script>
+
+</body>
+
+</html>
